355d4f90385948f4ec8c54e81210c2cd32c735b9
Hiroto Kitamura <kirikonknow@gmail.com>
2014-11-02 17:06:19 +0900

    jsonパースしてモデルクラス生成するところまで

--- ApiRegister/EntryList/EntryListParser.swift
+++ ApiRegister/EntryList/EntryListParser.swift
@@ -10,9 +10,53 @@ import UIKit
 
 class EntryListParser: NSObject {
    
-    func parse(json:AnyObject) {
+    func parse(json: AnyObject) -> Array<Entry>? {
         if let result = json as? Dictionary<String, Array<Dictionary<String, String>>> {
-            println(result)
+            return parseRoot(result["entries"])
+        } else {
+            return nil
         }
+    }
+    
+    private func parseRoot(entriesArray: Array<Dictionary<String, String>>?) -> Array<Entry>? {
+        if entriesArray == nil {
+            return nil
+        }
+        
+        var entryList: Array<Entry> = Array<Entry>()
+        
+        for entryDictionary: Dictionary<String, String> in entriesArray! {
+            let entry: Entry = parseEntry(entryDictionary)
+            
+            if (!entry.isValid()) {
+                return nil
+            }
+            
+            entryList.append(entry)
+        }
+        
+        for obj: Entry in entryList {
+            println(obj.title)
+        }
+        
+        return entryList
+    }
+    
+    private func parseEntry(entryDictionary: Dictionary<String, String>) -> Entry {
+        let entry: Entry = Entry()
+        
+        if entryDictionary["title"] != nil {
+            entry.title = entryDictionary["title"]
+        }
+        
+        if entryDictionary["url"] != nil {
+            entry.url = entryDictionary["url"]
+        }
+        
+        if entryDictionary["image"] != nil {
+            entry.image = entryDictionary["image"]
+        }
+        
+        return entry
     }
 }
