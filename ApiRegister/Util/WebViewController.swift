8e9aa2ba7f542dff5e97b07c7ca9b122716706c1
Hiroto Kitamura <kirikonknow@gmail.com>
2014-11-06 13:07:15 +0900

    戻る、進む、更新ボタンをwebviewに追加

--- ApiRegister/Util/WebViewController.swift
+++ ApiRegister/Util/WebViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class WebViewController: UIViewController, UIWebViewDelegate {
     
+    @IBOutlet var forwardButton: UIBarButtonItem!
+    @IBOutlet var backButton: UIBarButtonItem!
     @IBOutlet var webView: UIWebView!
     @IBOutlet var indicator: UIActivityIndicatorView!
     
@@ -33,6 +35,9 @@ class WebViewController: UIViewController, UIWebViewDelegate {
         let requestUrl = NSURL(string: mRootUrl)
         self.webView.loadRequest(NSURLRequest(URL:requestUrl!))
         self.webView.scalesPageToFit = true
+        
+        self.backButton.enabled = false
+        self.forwardButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,11 +48,37 @@ class WebViewController: UIViewController, UIWebViewDelegate {
     func webViewDidStartLoad(webView: UIWebView) {
         self.indicator.hidden = false
         self.indicator.startAnimating()
+        
+        if self.webView.canGoBack {
+            self.backButton.enabled = true
+        } else {
+            self.backButton.enabled = false
+        }
+        
+        if self.webView.canGoForward {
+            self.forwardButton.enabled = true
+        } else {
+            self.forwardButton.enabled = false
+        }
     }
     
     // webviewのロード終了
     func webViewDidFinishLoad(webView: UIWebView) {
         self.indicator.hidden = true
         self.indicator.stopAnimating()
+    }
+    
+    // 戻るボタン押した時
+    @IBAction func clickBackButton(sender: AnyObject) {
+        self.webView.goBack()
+    }
+    
+    // 進むボタンおした時
+    @IBAction func clickForwardButton(sender: AnyObject) {
+        self.webView.goForward()
+    }
+    
+    @IBAction func clickReloadButton(sender: AnyObject) {
+        self.webView.reload()
     }
 }
